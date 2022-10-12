b079cbd7d6681d604ca21184e4db0c69dd6d4ae3
arto-savolainen <111137161+arto-savolainen@users.noreply.github.com>
2022-10-13 01:48:52 +0300

    tehtävä 4.4
    4.4: apufunktioita ja yksikkötestejä, step2

---
+++ tests/totalLikes.test.js
@@ -0,0 +1,60 @@
+const listHelper = require('../utils/list_helper')
+
+describe('total likes', () => {
+  const emptyList = []
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const biggerList = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Bloopp',
+      author: 'Blabps',
+      url: 'www.blblasd.asd',
+      likes: 20,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: '400 likes blog',
+      author: 'Liked Man',
+      url: 'www.greatblog.org',
+      likes: 400,
+      __v: 0
+    }
+  ]
+
+
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes(emptyList)
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('of a bigger is calculated right', () => {
+    const result = listHelper.totalLikes(biggerList)
+    expect(result).toBe(420)
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,15 @@
 const dummy = (blogs) => {
-    return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) { return 0 }
+  return blogs.reduce((total, x) => {
+    return total + x.likes
+  }, 0)
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
