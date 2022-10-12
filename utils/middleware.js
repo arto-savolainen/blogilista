9b8496a9d45f2a3effa713a6d48d630af4c06dfc
arto-savolainen <111137161+arto-savolainen@users.noreply.github.com>
2022-10-13 00:49:03 +0300

    tehtävät 4.1 ja 4.2
    4.1 blogilista, step1
    4.2 blogilista, step2

---
+++ utils/middleware.js
@@ -0,0 +1,35 @@
+const morgan = require('morgan')
+const logger = require('./logger')
+
+morgan.token('postData', (req) => {
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return null
+    }
+  
+    return JSON.stringify(req.body)
+  })
+  
+const requestLogger = morgan(':method :url :status :res[content-length] - :response-time ms :postData')
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+const errorHandler = (error, request, response, next) => {
+    logger.error(error.message)
+    
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+    }
+  
+    next(error)
+  }
+
+module.exports = {
+    requestLogger,
+    unknownEndpoint,
+    errorHandler
+}
